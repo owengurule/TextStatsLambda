765a231840ff242a4567a9387d77c2cb54ccadae
SeedPaths-User <lessdismissive@email.com>
2014-09-09 08:32:33 -0600

    updates

---
+++ TextStatsLambda/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextStatsLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void TextStats(string input)
+        {
+            //print the # of chracters, #words # vowels, # consonants, # special characters
+
+            int numChars = input.Length;
+            int numWords = input.Split(' ').ToList().Count;
+            //declare intergers to hold the # of character types
+            int numVowels = 0;
+            int numCons = 0;
+            int numSpec = 0;
+
+            
+            //looped through all letters, print output
+            Console.WriteLine(input);
+            Console.WriteLine("# Chars: " + numChars);
+            Console.WriteLine("# Words: " + numWords);
+            Console.WriteLine("# Vowels: " + numVowels);
+            Console.WriteLine("# Consonants: " + numCons);
+            Console.WriteLine("# Spec Chars: " + numSpec);
+        }
+        
+        }
+}
